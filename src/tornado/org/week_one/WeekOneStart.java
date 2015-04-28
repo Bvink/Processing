794d7e1d03c39d25cb02776d3171d411b47d9f77
Brandon Vink <vinkbrandon@gmail.com>
2015-04-28 14:17:08 +0200

    Bars!

--- src/tornado/org/week_one/WeekOneStart.java
+++ src/tornado/org/week_one/WeekOneStart.java
@@ -4,49 +4,68 @@ import processing.core.PApplet;
 
 public class WeekOneStart extends PApplet {
 
-    int distX = 50;
-    int distY = 300;
+    int[] values = {15, 23, 58, 52, 35, 24, 74, 22, 32, 11, 23, 64};
 
     final int mulitplicationFactor = 3;
     final int barWidth = 20;
-    final int calcWidth = barWidth*2;
+    final int calcWidth = barWidth * 2;
     final int lineEvery = 10;
+    final int textOffset = 20;
     final int valueOffset = 20;
+    final int black = 0;
+    final int white = 255;
 
-    int[] values ={15, 23, 58, 52, 35, 24, 74, 22};
+    int distX = calcWidth * values.length;
+    int distY = (int) Math.floor(getHighestVal(values)) * mulitplicationFactor;
+    int offsetX = 50;
+    int offsetY = 50;
+
 
     public void setup() {
-        size(400,400);
-        background(255);
+        size(distX + (offsetX * 2), distY + (offsetY * 2));
+        System.out.println(lineEvery * values.length * mulitplicationFactor);
+        background(white);
         noLoop();
     }
 
-    public void draw () {
+    public void draw() {
 
-        stroke(0, 0, 0);
+        stroke(black);
 
 
-        for (int i = 0; i < (calcWidth * values.length); i+=calcWidth) {
-            float gray = map(i/calcWidth, 0, values.length, 0, 255);
+        for (int i = 0; i < (calcWidth * values.length); i += calcWidth) {
+            float gray = map(i / calcWidth, 0, values.length, black, white);
             fill(gray);
-            rect (i+distX+barWidth, distY, barWidth, -values[i/calcWidth]*mulitplicationFactor);
+            rect(i + offsetX,
+                    distY + offsetY,
+                    barWidth,
+                    -values[i / calcWidth] * mulitplicationFactor);
+
             fill(0);
-            text (String.valueOf(values[i/calcWidth]), i+distX+(barWidth), distY+valueOffset);
+
+            text(String.valueOf(values[i / calcWidth]),
+                    i + offsetX + (barWidth),
+                    distY + offsetY + valueOffset);
         }
 
-        stroke(0, 0, 0);
+        stroke(black);
 
-        for (int i = 0; i <= Math.floor(getHighestVal(values)/lineEvery); i++) {
-            line (distX+barWidth, distY - lineEvery*i*mulitplicationFactor,
-                    distX+values.length*calcWidth, distY - lineEvery*i*mulitplicationFactor);
-            text(i == 0 ? "00" : String.valueOf(i*lineEvery), distX, distY - lineEvery*i*mulitplicationFactor);
+        for (int i = 0; i <= Math.floor(getHighestVal(values) / lineEvery); i++) {
+            line(offsetX + barWidth,
+                    distY + offsetY - lineEvery * i * mulitplicationFactor,
+                    offsetX + values.length * calcWidth,
+                    distY + offsetY - lineEvery * i * mulitplicationFactor);
+
+            text(String.valueOf(i * lineEvery),
+                    offsetX - textOffset,
+                    distY + offsetY - lineEvery * i * mulitplicationFactor);
         }
     }
 
     private int getHighestVal(int[] arr) {
         int max = Integer.MIN_VALUE;
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] > max) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
                 max = arr[i];
             }
         }
